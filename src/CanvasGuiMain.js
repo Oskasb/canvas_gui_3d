082a01bc9605e8f299e422075d6530857544e90d
oskar.asbrink@gmail.com <oskar.asbrink@gmail.com>
2014-10-14 18:04:08 +0200

    move setup from gui to app

--- src/CanvasGuiMain.js
+++ src/CanvasGuiMain.js
@@ -1,12 +1,12 @@
 "use strict";
 
 define([
-	'application/EventManager',
+	'gui/GuiConfigLoader',
 	'gui/CanvasGuiState',
 	'gui/CanvasCalls'
 ],
 	function(
-		event,
+		GuiConfigLoader,
 		CanvasGuiState,
 		CanvasCalls
 		) {
@@ -15,17 +15,23 @@ define([
 		var uiResolution = 1024;
 
 
-		var CanvasGuiMain = function(camera, callbackMap) {
+		var CanvasGuiMain = function() {
+			this.guiConfigLoader = new GuiConfigLoader();
+		};
+
+		CanvasGuiMain.prototype.loadMasterConfig = function(masterUrl, ok, fail) {
+			this.guiConfigLoader.initConfigs(masterUrl, ok, fail);
+		};
+
+		CanvasGuiMain.prototype.initCanvasGui = function(camera, callbackMap) {
 			this.canvasCalls = new CanvasCalls(camera, uiResolution, callbackMap);
-
 			this.canvasGuiState = new CanvasGuiState(this.canvasCalls);
-
 			var reset = function() {
 				this.canvasGuiState.rebuildGuiLayers();
 			}.bind(this);
-
 			this.canvasCalls.registerResetCallback(reset);
 		};
+
 
 		CanvasGuiMain.prototype.setMainUiState = function(state) {
 			this.canvasGuiState.loadMainState(state);
