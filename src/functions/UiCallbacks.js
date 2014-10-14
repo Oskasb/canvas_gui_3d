472183c45b18055c901dc85bbf3af47b01af8d9a
oskar.asbrink@gmail.com <oskar.asbrink@gmail.com>
2014-10-14 16:34:49 +0200

    add UiCallback container class

---
+++ src/functions/UiCallbacks.js
@@ -0,0 +1,35 @@
+"use strict";
+
+define([],
+	function() {
+
+		var UiCallbacks = function() {};
+
+
+		var drawCallbacks = {
+
+		};
+
+
+
+		UiCallbacks.registerDrawCallback = function(id, callback) {
+			drawCallbacks[id] = callback;
+		};
+
+		UiCallbacks.addDrawCallbacks = function(callbackMap) {
+			for (var key in callbackMap) {
+				UiCallbacks.registerDrawCallback(key, callbackMap[key]);
+			}
+		};
+
+		UiCallbacks.getCallById = function(id) {
+			if (!drawCallbacks[id]) {
+				console.log("No Ui callback with id: ", id)
+				return function() {};
+			}
+			return drawCallbacks[id]
+		};
+
+
+		return UiCallbacks
+	});
